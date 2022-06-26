593e0457e05fb5b85b12e3b8daa7fc802827c0a4
Jang-GO <88973153+Jang-GO@users.noreply.github.com>
2022-06-26 15:57:38 +0900

    Add files via upload
    My first crawling code(static crawling)

---
+++ 로또번호 크롤링.py
@@ -0,0 +1,17 @@
+import requests
+import bs4
+
+URL = 'https://dhlottery.co.kr/gameResult.do?method=byWin'
+raw = requests.get(URL)
+
+html = bs4.BeautifulSoup(raw.text,'html.parser')
+
+target = html.find("div",{'class':'nums'})
+balls = target.find_all("span",{'class':'ball_645'})
+#print(target)
+
+print("<최근 로또 당첨번호>")
+for ball in balls[:-1]:
+    print("당첨번호: ",ball.text)
+
+print("보너스번호: ",balls[-1].text)
